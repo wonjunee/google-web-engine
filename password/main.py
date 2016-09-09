74d356ce919847494b5f3320a8b3c86f70849265
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-09-09 16:25:22 -0400

    dropdown buttons

--- password/main.py
+++ password/main.py
@@ -16,20 +16,10 @@ import webapp2
 
 form = """
 <form>
-	<label>
-		One
-		<input type="radio" name="q" value="one">
-	</label>
-
-	<label>
-		Two
-		<input type="radio" name="q" value="two">
-	</label>
-
-	<label>
-		Three
-		<input type="radio" name="q" value="three">
-	</label>
+	<select name="q">
+		<option>one</option>
+		<option>two</option>
+		<option>three</option>
 	<br>
 	<input type="submit">
 </form>
