3f46ed829cebefd387b02eeb2b7f4492fda804d0
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-09-10 10:15:16 -0400

    added html lines

--- template/main.py
+++ template/main.py
@@ -1,63 +1,26 @@
-from validation import *
-from escape_html import *
+import os
+
 import webapp2
 
-form = """
-<form method="post">
-	What is your birthday?
-	<br>
-
-	<label> Month
-		<input type="text" name="month" value="%(month)s">
-	</label>
-
-	<label> Day
-		<input type="text" name="day" value="%(day)s">
-	</label>
-
-	<label> Year
-		<input type="text" name="year" value="%(year)s">
-	</label>
-
-	<div style="color: red">%(error)s</div>
-	<br>
-	<br>
-	<input type="submit">
+form_html = """
+<form>
+<h2>Add a Food</h2>
+<input type="text" name="food">
+%s
+<button>Add</button>
 </form>
 """
 
-class MainPage(webapp2.RequestHandler):
-	def write_form(self, error="", month="", day="", year=""):
-		self.response.out.write(form % {"error": error,
-										"month": escape_html(month),
-										"day": escape_html(day),
-										"year": escape_html(year)})
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
 
+class MainPage(Handler):
 	def get(self):
-	    self.write_form()
-
-	def post(self):
-		user_month = self.request.get('month')
-		user_day = self.request.get('day')
-		user_year = self.request.get('year')
-
-		month = valid_month(user_month)
-		day = valid_day(user_day)
-		year = valid_year(user_year)
-
-		if not (month and day and year):
-			self.write_form("That doesn't look valid to me, mate.", user_month, user_day, user_year)
-		else:
-			self.redirect("/thanks")
-
-# Implementing redirection
-class ThanksHandler(webapp2.RequestHandler):
-	def get(self):
-		self.response.out.write("Thanks mate!")
+		self.write(form_html)
 
 app = webapp2.WSGIApplication([
 
-	('/', MainPage),
-	('/thanks', ThanksHandler)
+	('/', MainPage)
 
 ], debug=True)
