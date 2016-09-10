94ab2098116728609a89bb19f6432fc77cec7afc
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-09-10 10:30:28 -0400

    shopping list using jinja

---
+++ shopping-list-jinja2/main.py
@@ -0,0 +1,56 @@
+import os
+
+import webapp2
+
+form_html = """
+<form>
+<h2>Add a Food</h2>
+<input type="text" name="food">
+%s
+<button>Add</button>
+</form>
+"""
+
+hidden_html = """
+<input type="hidden" name="food" value="%s">
+"""
+item_html = "<li>%s</li>"
+
+shopping_list_html = """
+<br>
+<br>
+<h2>Shopping List</h2>
+<ul>
+%s
+</ul>
+"""
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a, **kw):
+		self.response.out.write(*a, **kw)
+
+class MainPage(Handler):
+	def get(self):
+		output = form_html
+		output_hidden = ""
+
+		# get_all: get all of food parameters and get them in the list
+		items = self.request.get_all("food")
+		if items:
+			output_items = ""
+			for item in items:
+				output_hidden += hidden_html % item
+				output_items += item_html % item
+
+			output_shopping = shopping_list_html % output_items
+			output += output_shopping
+
+		output = output % output_hidden
+
+		self.write(output)
+
+app = webapp2.WSGIApplication([
+
+	('/', MainPage)
+
+], debug=True)
