3e921817c0335f64924af077a2eb0e4dcfaa6330
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-09-10 09:40:24 -0400

    first commit

---
+++ happy-birthday/escape_html.py
@@ -0,0 +1,16 @@
+import cgi
+def escape_html(s):
+	# for (i, o) in (("&", "%amp;"),
+	# 			   (">", "&gt;"),
+	# 			   ("<", "&lt;"),
+	# 			   ('"', "&quote;")):
+	# 	s = s.replace(i,o)
+	# return s
+	return cgi.escape(s, quote = True)
+
+s = "<b>html</b>&lt;"
+
+print escape_html(s)
+
+from HTMLParser import HTMLParser
+print HTMLParser.unescape.__func__(HTMLParser,s)
