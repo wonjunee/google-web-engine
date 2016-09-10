c0886788c521307a83c0f2ab9fd4b016b3f2cdd2
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-09-10 10:36:19 -0400

    first commit

--- shopping-list-jinja2/main.py
+++ shopping-list-jinja2/main.py
@@ -1,6 +1,10 @@
 import os
 
+import jinja2
 import webapp2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 form_html = """
 <form>
@@ -28,6 +32,13 @@ shopping_list_html = """
 class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
+
+	def render_str(self, template, **params):
+		t = jinja_env.get_template(template)
+		return t.render(params)
+
+	def render(self, template, **kw):
+		self.write(self.render_str(template, **kw))
 
 class MainPage(Handler):
 	def get(self):
