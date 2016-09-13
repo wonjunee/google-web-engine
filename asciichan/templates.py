6b80b8f27c4a739da56097b6a86e053226c25894
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-09-13 13:50:55 -0400

    fixed bugs

--- asciichan/templates.py
+++ asciichan/templates.py
@@ -3,10 +3,10 @@ import re
 import jinja2
 import webapp2
 
-from google.appengine.ext import debug
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_evn = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 								autoescape=True)
 
 class Handler(webapp2.RequestHandler):
@@ -20,9 +20,38 @@ class Handler(webapp2.RequestHandler):
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
+class Art(db.Model):
+	# defining type. required makes sure the data is not empty
+	title = db.StringProperty(required = True)
+	art = db.TextProperty(required = True)
+
+	# auto_now_add: current time
+	created = db.DateTimeProperty(auto_now_add = True)
+
 class MainPage(Handler):
+	def render_front(self, title="", art="", error=""):
+		# Gql always starts from select *
+		arts = db.GqlQuery("select * from Art order by created DESC")
+		self.render("front.html", title=title, art=art, error=error, arts=arts)
+
 	def get(self):
-		self.render("front.html")
+		self.render_front()
+
+	def post(self):
+		title = self.request.get("title")
+		art = self.request.get("art")
+
+		if title and art:
+			# don't need to put datetime because
+			# it will be created automatically
+			a = Art(title=title, art=art)
+			a.put()
+
+			# to avoid reload error message
+			self.redirect("/")
+		else:
+			error = "we need both a title and some artwork!"
+			self.render_front(title, art, error)
 
 app = webapp2.WSGIApplication([
 
