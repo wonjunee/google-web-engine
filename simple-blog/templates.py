a43c9e243c4b790b8203b5c82b72a141b9a5e9e6
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-09-13 14:12:01 -0400

    updated

--- simple-blog/templates.py
+++ simple-blog/templates.py
@@ -20,6 +20,47 @@ class BlogHandler(webapp2.RequestHandler):
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
+class MainPage(BlogHandler):
+	def get(self):
+		self.write("Hello, Udacity!")
+
+
+###### blog stuff
+
+def blog_key(name = 'default'):
+	return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+	subject = db.StringProperty(required = True)
+	content = db.TextProperty(required = True)
+	created = db.DateTimeProperty(auto_now_add = True)
+	last_modified = db.DateTimeProperty(auto_now = True)
+
+	def render(self):
+		self._render_text = self.content.replace('\n', '<br>')
+		return render_str("post.html", p = self)
+
+class BlogFront(BlogHandler):
+	def get(self):
+		posts = Post.all().order('-created')
+		self.render('front.html', posts = posts)
+
+class PostPage(BlogHandler):
+	def get(self, post_id):
+		key = db.GqlQuery("select * from Post order by created desc limit 10")
+		self.render('front.html', posts = posts)
+
+class PostPage(BlogHandler):
+	def get(self, post_id):
+		key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+		post = db.get(key)
+
+		if not post:
+			self.error(404)
+			return
+
+		self.render("permalink.html", post = post)
+		
 class Art(db.Model):
 	# defining type. required makes sure the data is not empty
 	title = db.StringProperty(required = True)
