11f3c6ea60e48e23a4a5627bbdd46ce3eae8d278
Wonjun Lee <wonjunlee@Wonjuns-MacBook.local>
2016-09-14 11:54:17 -0400

    hash for password

---
+++ cookie/hash.py
@@ -0,0 +1,6 @@
+import hashlib
+
+def hash_str(s):
+	val = h.split(',')[0]
+	if h == make_secure_val(val):
+		return val
--- cookie/templates.py
+++ cookie/templates.py
@@ -9,7 +9,7 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 								autoescape=True)
 
-class BlogHandler(webapp2.RequestHandler):
+class Handler(webapp2.RequestHandler):
 	def write(self, *a, **kw):
 		self.response.out.write(*a, **kw)
 
@@ -20,80 +20,23 @@ class BlogHandler(webapp2.RequestHandler):
 	def render(self, template, **kw):
 		self.write(self.render_str(template, **kw))
 
-class MainPage(BlogHandler):
+class MainPage(Handler):
 	def get(self):
-		self.write("Hello, Udacity!")
-
-
-###### blog stuff
-
-def blog_key(name = 'default'):
-	return db.Key.from_path('blogs', name)
-
-class Post(db.Model):
-	subject = db.StringProperty(required = True)
-	content = db.TextProperty(required = True)
-	created = db.DateTimeProperty(auto_now_add = True)
-	last_modified = db.DateTimeProperty(auto_now = True)
-
-	def render(self):
-		self._render_text = self.content.replace('\n', '<br>')
-		return render_str("post.html", p = self)
-
-class BlogFront(BlogHandler):
-	def get(self):
-		posts = Post.all().order('-created')
-		self.render('front.html', posts = posts)
-
-class PostPage(BlogHandler):
-	def get(self, post_id):
-		key = db.GqlQuery("select * from Post order by created desc limit 10")
-		self.render('front.html', posts = posts)
-
-class PostPage(BlogHandler):
-	def get(self, post_id):
-		key = db.Key.from_path('Post', int(post_id), parent=blog_key())
-		post = db.get(key)
-
-		if not post:
-			self.error(404)
-			return
-
-		self.render("permalink.html", post = post)
-		
-class Art(db.Model):
-	# defining type. required makes sure the data is not empty
-	title = db.StringProperty(required = True)
-	art = db.TextProperty(required = True)
-
-	# auto_now_add: current time
-	created = db.DateTimeProperty(auto_now_add = True)
-
-class MainPage(BlogHandler):
-	def render_front(self, title="", art="", error=""):
-		# Gql always starts from select *
-		arts = db.GqlQuery("select * from Art order by created DESC")
-		self.render("front.html", title=title, art=art, error=error, arts=arts)
-
-	def get(self):
-		self.render_front()
-
-	def post(self):
-		title = self.request.get("title")
-		art = self.request.get("art")
-
-		if title and art:
-			# don't need to put datetime because
-			# it will be created automatically
-			a = Art(title=title, art=art)
-			a.put()
-
-			# to avoid reload error message
-			self.redirect("/")
+		self.response.headers['Content-Type'] = 'text/plain'
+		visits = self.request.cookies.get('visits', '0')
+		# make sure visits is an int
+		if visits.isdigit():
+			visits = int(visits) + 1
 		else:
-			error = "we need both a title and some artwork!"
-			self.render_front(title, art, error)
+			visits = 0
 
+		# Adding header to Set-Cookie:visits = visits
+		self.response.headers.add_header('Set-Cookie', 'visits=%s' % visits)
+
+		if visits > 10:
+			self.write("You are the beset ever!")
+		else:
+			self.write("You've been here %s times!" % visits)
 app = webapp2.WSGIApplication([
 
 	('/', MainPage)
